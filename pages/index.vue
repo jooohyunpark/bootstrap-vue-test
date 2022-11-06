e8270215821c5c8c79f5835a7ab16240003b7c68
jooohyunpark <jhp527@nyu.edu>
2022-11-06 15:18:53 -0500

    format

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,33 @@
 <template>
-  <div>
-
-  <b-container>
-  <b-row>
-    <b-col md="4"><div class="box"></div></b-col>
-    <b-col md="4"><div class="box"></div></b-col>
-        <b-col md="4"><div class="box"></div></b-col>
-  </b-row>
-
-
-</b-container>
-
-</div>
+  <div class="page">
+    <b-container>
+      <b-row>
+        <b-col cols="12" md="6" lg="4"><div class="box"></div></b-col>
+        <b-col cols="12" md="6" lg="4"><div class="box"></div></b-col>
+        <b-col cols="12" md="6" lg="4"><div class="box"></div></b-col>
+        <b-col cols="12" md="6" lg="4"><div class="box"></div></b-col>
+        <b-col cols="12" md="6" lg="4"><div class="box"></div></b-col>
+        <b-col cols="12" md="6" lg="4"><div class="box"></div></b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+};
 </script>
 
-<style lang="scss" scoped >
-.box{
-  height: 50px;
-  background: gray;
-  width: 100%;
+<style lang="scss" scoped>
+.page {
+  margin: 100px 0;
+
+  .box {
+    height: 50px;
+    background: gray;
+    width: 100%;
+    margin-bottom: 40px;
+  }
 }
 </style>
